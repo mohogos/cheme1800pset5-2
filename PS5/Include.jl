78e75a076a46b132354e93e5e4bdcbac1d4bab3b
mohogos <124085079+mohogos@users.noreply.github.com>
2023-05-12 12:44:13 -0400

    summary

---
+++ PS5/Include.jl
@@ -0,0 +1,17 @@
+# Setup paths -
+const _ROOT = pwd();
+const _PATH_TO_DATA = joinpath(_ROOT, "data")
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+
+# packages that we need -
+using CSV
+using DataFrames
+using LinearAlgebra
+using Statistics
+using Distributions
+using PrettyTables
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC,"Files.jl"))
+include(joinpath(_PATH_TO_SRC,"Compute.jl"))
+
